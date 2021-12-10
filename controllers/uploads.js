79615733de876761a44d62b0d9f2b8a0b9584d1e
Tibaran <palacios.tibaran@gmail.com>
2021-12-09 22:12:49 -0500

    primer commit

---
+++ controllers/uploads.js
@@ -0,0 +1,207 @@
+const path = require('path');
+const fs   = require('fs');
+
+const cloudinary = require('cloudinary').v2
+cloudinary.config( process.env.CLOUDINARY_URL );
+
+const { response } = require('express');
+const { subirArchivo } = require('../helpers');
+
+const { Usuario, Producto } = require('../models');
+
+
+const cargarArchivo = async(req, res = response) => {
+
+
+    try {
+        
+        // txt, md
+        // const nombre = await subirArchivo( req.files, ['txt','md'], 'textos' );
+        const nombre = await subirArchivo( req.files, undefined, 'imgs' );
+        res.json({ nombre });
+
+    } catch (msg) {
+        res.status(400).json({ msg });
+    }
+
+}
+
+
+const actualizarImagen = async(req, res = response ) => {
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch ( coleccion ) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if ( !modelo ) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${ id }`
+                });
+            }
+        
+        break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if ( !modelo ) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${ id }`
+                });
+            }
+        
+        break;
+    
+        default:
+            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+    }
+
+
+    // Limpiar imágenes previas
+    if ( modelo.img ) {
+        // Hay que borrar la imagen del servidor
+        const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
+        if ( fs.existsSync( pathImagen ) ) {
+            fs.unlinkSync( pathImagen );
+        }
+    }
+
+
+    const nombre = await subirArchivo( req.files, undefined, coleccion );
+    modelo.img = nombre;
+
+    await modelo.save();
+
+
+    res.json( modelo );
+
+}
+
+
+const actualizarImagenCloudinary = async(req, res = response ) => {
+
+    const { id, coleccion } = req.params;
+    
+
+    let modelo;
+    let tipoCarpeta;
+    let nombreCarpeta;
+
+    switch ( coleccion ) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            tipoCarpeta = "usuarios";
+            
+            if ( !modelo ) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${ id }`
+                });
+            }
+            nombreCarpeta = modelo._id;
+        
+        break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            tipoCarpeta = "productos";
+            if ( !modelo ) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${ id }`
+                });
+            }
+            nombreCarpeta = modelo._id;
+        
+        break;
+    
+        default:
+            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+    }
+
+    
+
+
+    // Limpiar imágenes previas
+    if ( modelo.img ) {
+        const nombreArr = modelo.img.split('/');
+        const nombre    = nombreArr[ nombreArr.length - 1 ];
+        const [ public_id ] = nombre.split('.');
+        cloudinary.uploader.destroy( `${tipoCarpeta}/${nombreCarpeta}/${public_id}` );
+    }
+
+
+
+    try {
+        const { tempFilePath } = req.files.archivo
+        const { secure_url } = await cloudinary.uploader.upload( tempFilePath, {folder: `${tipoCarpeta}/${nombreCarpeta}/`} );
+        console.log(secure_url);
+        modelo.img = secure_url;
+
+        await modelo.save();
+
+
+        res.json( modelo );
+
+
+    }catch(msg){
+        res.status(400).json({ msg });
+    }
+    
+    
+}
+
+const mostrarImagen = async(req, res = response ) => {
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch ( coleccion ) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if ( !modelo ) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${ id }`
+                });
+            }
+        
+        break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if ( !modelo ) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${ id }`
+                });
+            }
+        
+        break;
+    
+        default:
+            return res.status(500).json({ msg: 'Se me olvidó validar esto'});
+    }
+
+
+    // Limpiar imágenes previas
+    if ( modelo.img ) {
+        // Hay que borrar la imagen del servidor
+        const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
+        if ( fs.existsSync( pathImagen ) ) {
+            return res.sendFile( pathImagen )
+        }
+    }
+
+    const pathImagen = path.join( __dirname, '../assets/no-image.jpg');
+    res.sendFile( pathImagen );
+}
+
+
+
+
+module.exports = {
+    cargarArchivo,
+    actualizarImagen,
+    mostrarImagen,
+    actualizarImagenCloudinary
+}
